c74011dff84790500e8f80e98b08e59a9fbedd52
Rahul Bera <bera2591@gmail.com>
2020-12-23 12:13:18 +0530

    carica_bot.py: Create a class 'CaricaBot' that extends discord.Client

--- carica_bot.py
+++ carica_bot.py
@@ -1,4 +1,19 @@
 """Main code for the Carica-Bot
 """
 
-print('Caricare')
+import discord
+
+class CaricaBot(discord.Client):
+    """CaricaBot class that extends the discord.Client class
+    & overwrites the methods of the extended class
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        """Print bot details & send User details to the 'caricare' channel
+        when bot is ready.
+        """
+        print(f'Logged in as {self.user.name} - {self.user.id}')
+        print('------')
