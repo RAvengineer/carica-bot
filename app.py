c2ad784229e12fcf2e6f371062ec6a42fa2c68e2
Rahul Bera <bera2591@gmail.com>
2020-12-25 11:03:21 +0530

    app.py: Use multiprocessing to initiate the bot CaricaBot.run is a blocking method, thus holding the code when called. In order to avoid that, it is called in a seperate process, thus allowing to process the rest of the code.

--- app.py
+++ app.py
@@ -2,6 +2,21 @@
 
 from dotenv import load_dotenv
 from os import getenv
+from carica_bot import CaricaBot
+from multiprocessing import Process
 
 load_dotenv()
+# Variables
 BOT_TOKEN = getenv('BOT_TOKEN')
+
+
+try:
+    # CaricaBot.run is a blocking method, thus holding the code when
+    # called. In order to avoid that, it is called in a seperate process,
+    # thus allowing to process the rest of the code.
+    initiate_bot = Process(target=CaricaBot().run, args=(BOT_TOKEN,))
+    initiate_bot.start()
+except Exception as e:
+    print(f'Exception in app.py: {str(e)}')
+# initiate_bot.terminate()
+print('Done & dusted')
